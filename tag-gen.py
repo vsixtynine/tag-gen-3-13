17e6e58dfb71143a206004a8f1863104e36d55d7
Vlad <mail@vladonline.com>
2020-07-26 12:26:44 +0300

    initial

---
+++ tag-gen.py
@@ -0,0 +1,34 @@
+class Tag:
+    # ...
+
+class HTML:
+    # ...
+
+class TopLevelTag:
+    # ...
+
+
+if __name__ == "__main__":
+    with HTML(output=None) as doc:
+        with TopLevelTag("head") as head:
+            with Tag("title") as title:
+                title.text = "hello"
+                head += title
+            doc += head
+
+        with TopLevelTag("body") as body:
+            with Tag("h1", klass=("main-text",)) as h1:
+                h1.text = "Test"
+                body += h1
+
+            with Tag("div", klass=("container", "container-fluid"), id="lead") as div:
+                with Tag("p") as paragraph:
+                    paragraph.text = "another test"
+                    div += paragraph
+
+                with Tag("img", is_single=True, src="/icon.png") as img:
+                    div += img
+
+                body += div
+
+            doc += body
